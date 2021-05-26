fbc02d0f5c69b9daa71c244bcb11226d1c9523f9
Mahendersinngh <mahendersinngh@gmail.com>
2021-05-26 07:47:43 +0530

    updatedapp.java webhook

--- src/main/java/com/mahender/app/App.java
+++ src/main/java/com/mahender/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Learning Maven and Jenkins Poll SCM Job" );
+        System.out.println( "With WebHook" );
     }
 }
